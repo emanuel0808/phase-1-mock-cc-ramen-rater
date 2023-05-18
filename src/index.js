741096d22c31bfb039d34c68ef524d55804edfbc
Emanuel Gaona <gaonaemanuel7@gmail.com>
2023-05-17 18:05:20 -0700

    Adds index.html

--- src/index.js
+++ src/index.js
@@ -1,1 +1,37 @@
-// write your code here
+const ramenAPI =  'http://localhost:3000/ramens';
+const ramenMenuDiv = el('ramen-menu');
+
+
+
+
+fetch(ramenAPI)
+   .then(res => res.json())
+   .then(renderRamens);
+
+
+function renderRamens(ramens){
+    ramens.forEach(renderRamen);
+}
+
+function renderRamen(ramen) {
+    const ramenImageElement = document.createElement('img');
+    ramenImageElement.src = ramen.image;
+    ramenMenuDiv.append(ramenImageElement);
+
+
+    ramenImageElement.addEventListener('click', () => ramenClickHandler(ramen))
+}
+function ramenClickHandler(ramen) {
+   el('detail-image').src = ramen.image;
+   el('detail-name').textContent = ramen.name;
+   el('detail-restaurant').textContent = ramen.restaurant;
+   el('rating-display').textContent = ramen.rating;
+   el('comment-display').textContent = ramen.comment;
+  
+}
+
+
+
+function el(id) {
+   return document.getElementById(id);
+}
